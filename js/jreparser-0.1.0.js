94ffe2befc08c2b5ef2f6d15efb31b6554b414b3
邹润阳 <zry656565@gmail.com>
2014-05-06 19:52:28 +0800

    webpage version 0.1 done.

--- js/jreparser-0.1.0.js
+++ js/jreparser-0.1.0.js
@@ -1,29 +1,41 @@
 (function($){
     var JTester = function (r) {
-        var formatList = r.match(/^\/(.*)\/([igm]{0,3})$/i);
-        this.r = new RegExp(formatList[1], formatList[2]);
+        var begin = r.indexOf('/');
+        var end = r.lastIndexOf('/');
+        var flags = r.match(/\/([igm]{0,3})$/i)[1];
+        this.r = new RegExp(r.substring(begin + 1, end), flags);
+        console.log(flags + "\n" +this.r);
     };
 
     JTester.prototype = {
         constructor: JTester,
         exec: function (str) {
             var result,
-                counter = 1,
-                cell = $('<tr>');
-            if (this.r.global) {
-                while ((result = this.r.exec(str))!==null) {
-                    console.log(result);
-                    cell.append($('<td>').text(counter));
-                    cell.append($('<td>').text(result[0]));
-                    cell.append($('<td>').text(result.index));
-                    for (var i = 1; i < result.length; i++) {
-                        cell.append($('<td>').text(counter + '-' + i));
-                        cell.append($('<td>').text(result[i]));
-                    }
-                    counter++;
-                    $('#outputTable').append(cell);
-                    cell = $('<tr>');
+                counter = 1;
+
+            while ((result = this.r.exec(str))!==null) {
+                this.printToTable(counter, result);
+                counter++;
+                if (!this.r.global) {
+                    break;
                 }
+            }
+        },
+        printToTable: function (counter, result) {
+            var table = $('#outputTable');
+            var cell = $('<tr>');
+            cell.append($('<td>').text(counter));
+            cell.append($('<td>').text(result[0]));
+            cell.append($('<td>').text(result.index));
+            table.append(cell);
+            if (result.length > 1) {
+                cell = $('<tr>');
+                for (var i = 1; i < result.length; i++) {
+                    cell.append($('<td>').text(counter + '-' + i + ' -> SubString'));
+                    cell.append($('<td>').text(result[i]));
+                    cell.append($('<td>'));
+                }
+                table.append(cell);
             }
         }
     };
